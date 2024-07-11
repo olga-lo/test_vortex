44af2d56df1e43f5d6601a089c6acb8f5e20417d
Olga <olga.lobanova.com@gmail.com>
2024-07-11 20:38:52 +0600

    Добавлен третий тест

--- tests/test_currency_conversion.py
+++ tests/test_currency_conversion.py
@@ -13,36 +13,29 @@ def test_currency_conversion():
         amount_input = page.locator('[aria-label="Cумма"]')
         amount_input.fill("10")
 
-        # 3. Найти селектор валюты исходной и выбрать EUR
+        # 3. Найти селектор первой валюты и выбрать EUR
         converter_first_select = page.locator('[data-finance_media-desktop="converter_first_select"]')
         converter_first_select.click()
 
         first_currency = page.locator('[data-finance_media-desktop="EUR"]').first
         first_currency.click()
 
-        # 4
+        # 4 Найти селектор второй валюты и выбрать AUD
         converter_second_select = page.locator('[data-finance_media-desktop="converter_second_select"]')
         converter_second_select.click()
 
         second_currency = page.locator('[data-finance_media-desktop="AUD"]').nth(1)
         second_currency.click()
 
-
-        # 5. Найти кнопку кнопку конвертации и кликнуть
+        # 5. Найти кнопку конвертации и кликнуть
         button_converter = page.locator('[data-finance_media-desktop="convert_link"]')
         button_converter.click()
 
-        # # 6. Найти значение сконвертированного курса и вывести его значение
-        # result = page.locator('.converter-display__value')
-
-
+        # 6. Найти значение сконвертированного курса и вывести его
         value_of_converted_rate = page.locator('[class="uKWNQ"]')
-
-
         print(f"Сконвертированный курс: {value_of_converted_rate.inner_text()}")
-        #
-        # browser.close()
-        input("Press Enter to close the browser...")
+
+        browser.close()
 
 
 if __name__ == "__main__":
